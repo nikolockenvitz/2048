68d44eb9fece08dccfe9a0fae23a8208d8490d88
Niko Lockenvitz <n.lockenvitz@online.de>
2018-05-17 09:44:18 +0200

    Improved new game + added comments

--- 2048.py
+++ 2048.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
-# TODO: set grid, get scale in __init__ (fonts), rename and explain resize2
-
 FILENAME_HIGHSCORE = "2048highscore.txt"
 
 KEYS_UP    = ["w", "Up"]
@@ -47,6 +45,10 @@ from random import randint
 from math import log2
 import os
 
+"""
+Class UI
+This class implements the frontend using tkinter.
+"""
 class UI:
     def __init__(self):
         # set keys
@@ -98,6 +100,10 @@ class UI:
         self.root.mainloop()
 
     def adjustWindowToCurrentState(self, event=None):
+        """
+        When state of window changes (normal <-> zoomed) the content has to
+        be adjusted. Either default width or maximum width.
+        """
         # zoomed to normal
         if((self.unit == self.root.winfo_screenheight()//GRID_ROWS-2 or
             self.unit == self.root.winfo_screenwidth()//GRID_COLUMNS) and
@@ -112,24 +118,43 @@ class UI:
             self.setWindowSize(width)
 
     def adjustWindowToCurrentWidth(self, event=None):
-        # adjusts content and window size
+        """
+        After the user changed the window size, the window will be resized
+        to new width (which includes size of content). The size of the
+        window will not really change but the size of content.
+        """
         width = self.root.winfo_width()
         self.setWindowSize(width)
 
     def setWindowSize(self, width=DEFAULT_WIDTH_WINDOW):
+        """
+        This function resizes the window. Depending on parameter "width" the
+        size of a unit (in grid) is calculated. One unit should be at least
+        5 pixels (which is actually very small, but must not be larger than
+        possible. That means a maximum value for one unit is calculated
+        depending on screen size and grid size. (Also some space for task bar
+        should be available)
+        When maximum unit size is reached the window will automatically switch
+        to full screen (zoomed state).
+        When window size has to be changed, also all elements in UI have to be
+        adjusted.
+        """
+        # calculate unit, width and height
         self.unit = min(max(5, width // GRID_COLUMNS),
                         self.root.winfo_screenheight()//GRID_ROWS-2,
                         self.root.winfo_screenwidth()//GRID_COLUMNS)
         width  = GRID_COLUMNS * self.unit
         height = GRID_ROWS    * self.unit
 
+        # set state of window (zoomed/normal)
         if(self.unit == self.root.winfo_screenheight()//GRID_ROWS-2 or
            self.unit == self.root.winfo_screenwidth()//GRID_COLUMNS):
             # max value -> full screen / zoomed
             self.root.state("zoomed")
         else:
             self.root.state("normal")
-        
+
+        # update window (also UI-elements if changed)
         size = str(width) + "x" + str(height)
         self.root.geometry(size)
         if(width != self.width or height != self.height):
@@ -139,6 +164,12 @@ class UI:
             self.showUIElements()
 
     def confirmAction(self, msgboxHeading, msgboxText):
+        """
+        Critical actions need to be confirmed. This functions creates a
+        message box to ask the user whether he is sure to continue.
+        This will only happen if game is not finished, otherwise the action
+        can continue (because a finished game is not supposed to be critical).
+        """
         if(not self.game.isFinished()):
             # game is not finished yet
             answer = messagebox.askyesno(msgboxHeading,
@@ -149,24 +180,37 @@ class UI:
         return True
 
     def rootDestroy(self):
+        """
+        This functions closes the window gracefully when confirmed again.
+        """
         if(self.confirmAction("Quit?","Do you really want to quit?")):
             self.game.writeHighScore()
             self.root.destroy()
 
     def newGame(self, event=None):
+        """
+        After confirming this action, old game will be saved and a new game
+        will be created. Afterwards UI has to be updated.
+        """
         if(self.confirmAction("New Game?",
                               "Do you really want to start a new game?")):
             self.game.writeHighScore()
-            self.game = Game()
+            self.game.newGame()
             self.show()
 
     def labelField(self):
+        """
+        Factory method to create a field/tile containing the numbers.
+        """
         return Label(self.root,
                      text   = "",
                      anchor = CENTER,
                      font   = self.fontFields)
 
     def labelText(self, text=""):
+        """
+        Factory method to create a text label.
+        """
         return Label(self.root,
                      text = text,
                      bg   = self.bg,
@@ -174,6 +218,10 @@ class UI:
                      fg   = "#ffffff")
 
     def updateFontSize(self):
+        """
+        This function updates all font sizes (depending on new size of grid unit)
+        and labels.
+        """
         self.fontFields[1] = int(self.coefficientFontFields * self.unit)
         self.fontText[1]   = int(self.coefficientFontText   * self.unit)
         self.font2048[1]   = int(self.coefficientFont2048   * self.unit)
@@ -188,6 +236,10 @@ class UI:
                 field.config(font=self.fontFields)
 
     def showUIElements(self):
+        """
+        Labels and number fields are placed at their posiiton depending on grid.
+        Afterwards fonts and content will be updated.
+        """
         for i in range(4):
             self.listLabels[i].place(x =      26*self.unit,
                                      y = (6*i+1)*self.unit,
@@ -205,6 +257,9 @@ class UI:
         self.show()
 
     def hideUIElements(self):
+        """
+        During resize old labels and fields have to be removed.
+        """
         for label in self.listLabels:
             label.place_forget()
 
@@ -213,7 +268,11 @@ class UI:
                 field.place_forget()
 
     def createUIElements(self):
-        self.labelScore = self.labelText()
+        """
+        This function creates all number fields and labels and sets up general
+        settings for these (text, fonts, colours, bindings).
+        """
+        self.labelScore     = self.labelText()
         self.labelHighScore = self.labelText()
         
         self.label2048 = Label(self.root,
@@ -259,6 +318,11 @@ class UI:
         self.showUIElements()
 
     def keyPressed(self, event):
+        """
+        When a key is pressed, it will check whether pressed key should
+        trigger a move.
+        Also some additional keys for resize etc. can be processed here.
+        """
         for direction in range(4):
             if(event.keysym in self.keys[direction]):
                 self.game.move(direction)
@@ -272,12 +336,19 @@ class UI:
         #self.root.iconify()
 
     def getColours(self, number):
-        # return [exponent, fg, bg] for number
+        """
+        This functions returns a list [exponent, fg, bg] for passed number.
+        """
         if(number <= 0): number=1
         ix = int(log2(number))
         return self.colours[ix]
 
     def show(self):
+        """
+        This function updates content and appearence of fields.
+        Also current score and highscore are shown. Depending on current game
+        status (finished or not) the background colour is adjusted.
+        """
         for y in range(4):
             for x in range(4):
                 currentNumber = self.game.field[y][x]
@@ -300,33 +371,71 @@ class UI:
 
         self.root.update()
 
+"""
+Class Game
+This class implements the backend including following important functions:
+
+ - move(direction)
+    0 - move north / up
+    1 - move east  / right
+    2 - move south / down
+    3 - move west  / left
+ - readHighScore()
+ - writeHighScore()
+ - newGame()
+ - isFinished()
+
+"""
 class Game:
     def __init__(self):
+        # define probability of fours when random numbers appear (in percent)
         self.probability4 = 10
 
+        # construct filename of highscore file
         self.initFileName()
+
+        # initialize a new game
+        self.newGame()
+
+    def newGame(self):
+        # initialize/reset field and values
         self.initField()
         self.initValues()
 
     def initValues(self):
+        """
+        This function initializes all variables which are necessary beside
+        the field / numbers.
+        """
         self.score = 0 # max: 3932164
         self.round = 0
         self.readHighScore()
 
     def initFileName(self):
-        # get directory in which the file is located
+        """
+        This function gets filepath/-name for highscore file. It has to be
+        in same directory in which the script is.
+        """
+        # get directory in which the script/file (2048.py) is located
         pathDir = os.path.dirname(os.path.abspath(__file__))
 
         # append specified filename to directory of script
         self.filename = pathDir + "\\" + FILENAME_HIGHSCORE
 
     def initField(self):
+        """
+        This function creates an empty field and inserts two random numbers.
+        """
         self.field  = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 
         self.insertRandomNumber()
         self.insertRandomNumber()
 
     def readHighScore(self):
+        """
+        This function tries to read a high score from file. When this is not
+        possible the highscore is initalized.
+        """
         try:
             with open(self.filename, "r") as f:
                 self.highscore = int(f.readlines()[0].strip())
@@ -336,6 +445,9 @@ class Game:
             self.highscore = 0
 
     def writeHighScore(self):
+        """
+        This functions tries to write the high score in the file.
+        """
         self.highscore = max(self.highscore, self.score)
         try:
             with open(self.filename, "w") as f:
@@ -346,8 +458,14 @@ class Game:
             print("***", e)
 
     def move(self, direction):
-        # direction has to be 0,1,2,3 (N,E,S,W)
-        # return True / False
+        """
+        This function implements the fundamental part of the game.
+        It moves and merges the tiles in passed direction.
+        Direction has to be 0,1,2,3 (N,E,S,W)
+        When move was successful (something changed/merged) a new number
+        will be inserted, round will be incremented, highscore updated and
+        True will be returned.
+        """
         new = [self.__move_north,
                self.__move_east,
                self.__move_south,
@@ -364,7 +482,7 @@ class Game:
     def __move_north(self):
         new = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
         for x in range(0,4):
-            # 1. get list without empty fields
+            # 1. get list without empty fields for each column (just numbers)
             values = []
             for y in range(0,4):
                 if(self.field[y][x] != 0): values.append(self.field[y][x])
@@ -386,7 +504,7 @@ class Game:
     def __move_west(self):
         new = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
         for y in range(0,4):
-            # 1. get list without empty fields
+            # 1. get list without empty fields for each row (just numbers)
             values = []
             for x in range(0,4):
                 if(self.field[y][x] != 0): values.append(self.field[y][x])
@@ -408,7 +526,7 @@ class Game:
     def __move_south(self):
         new = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
         for x in range(0,4):
-            # 1. get list without empty fields
+            # 1. get list without empty fields for each column (just numbers)
             values = []
             for y in range(3,-1,-1):
                 if(self.field[y][x] != 0): values.append(self.field[y][x])
@@ -430,7 +548,7 @@ class Game:
     def __move_east(self):
         new = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
         for y in range(0,4):
-            # 1. get list without empty fields
+            # 1. get list without empty fields for each row (just numbers)
             values = []
             for x in range(3,-1,-1):
                 if(self.field[y][x] != 0): values.append(self.field[y][x])
@@ -450,7 +568,16 @@ class Game:
         return new
 
     def insertRandomNumber(self):
-        # insert 2 or 4 at random field (must be free)
+        """
+        This function inserts 2 or 4 at random field (must be free).
+        Empty fields are numbered from 1 to n from top left to bottom right
+        corner. A random number decides which of these fields is chosen. The
+        functions loops through the field line by line and counts number of
+        empty fields to find chosen field. Which number has to be inserted
+        will be decided by randomness again (using previously defined
+        probabilty for a 4). At the end coordinates of field where new number
+        was inserted will be returned.
+        """
         nulls = self.getNumNullValues()
         if(nulls == 0): return False
         r = randint(1, nulls)
@@ -467,6 +594,9 @@ class Game:
                         return (y,x)
 
     def getNumNullValues(self):
+        """
+        This function returns number of empty fields
+        """
         num = 0
         for row in self.field:
             for number in row:
@@ -474,6 +604,13 @@ class Game:
         return num
 
     def isFinished(self):
+        """
+        This functions returns whether game is finshed or not.
+        A game is finished when no more move can be done. This is obviously
+        not the case when there are empty fields (at least one). A move is
+        also possible when some adjacent fields have same values and can be
+        merged (Von Neumann neighborhood).
+        """
         if(self.getNumNullValues() == 0):
             # every field is filled, now check for possible merging
             for y in range(0,4):
@@ -490,6 +627,10 @@ class Game:
             return False
 
     def show(self):
+        """
+        This show function prints current game status and is only used
+        for debugging.
+        """
         print("Score:", self.score,
               "Round:", self.round,
               "HighScore:", self.highscore,
